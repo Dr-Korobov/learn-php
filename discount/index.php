2180b652be3fc20cff2f1bc6a65755a25765c6a6
Danil Korobov <dkorobov555@gmail.com>
2020-04-16 20:58:31 +0300

    Discount

--- discount/index.php
+++ discount/index.php
@@ -15,13 +15,29 @@
                 exit;
              }        
             
-
-            //echo $product, $price, $qty, $total, $discount;
-            echo "$twd \n $total";
-
-
+            if (!is_numeric($discount)) {
+                $twd=$total;
+            }
 
                  //localhost:8080/?product=antiseptic&price=50&qty=2&discount=30 
         ?>        
+            <table border= "2">
+                <caption>RECEIPT</caption>
+                <tr>
+                    <th>Product Name</th>
+                    <th>Unit Cost</th>
+                    <th>Quantity</th>
+                    <th>Total</th>
+                    <th>Total with discount</th>
+                </tr>
+                <tr>
+                    <td><?php echo $product ?></td>
+                    <td><?php echo $price ?></td>
+                    <td><?php echo $qty ?></td>
+                    <td><?php echo $total ?></td>
+                    <td><?php echo $twd ?></td>
+                </tr>
+
+            </table>
     </body>
 <html>
