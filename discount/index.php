f31f4c351d6e31f85cdb9ca4924b28af6ca2a885
Danil Korobov <dkorobov555@gmail.com>
2020-04-20 16:38:10 +0300

    Discount_NEW

--- discount/index.php
+++ discount/index.php
@@ -10,14 +10,25 @@
                     //total with discount - twd;
             $twd= $total*(1-$discount/100);
 
-            if ($price <= 0 ||$qty <= 0 || !is_numeric($price) || !is_numeric($qty)){
-                echo 'Invalid value';
+            if (!is_numeric($price)){
+                echo 'Price is not a number!';
                 exit;
              }        
-            
-            if (!is_numeric($discount)) {
-                $twd=$total;
-            }
+
+            if (!is_numeric($qty)){
+                echo 'Quantity is not a number!';
+                exit;
+             }  
+
+            if ($price <= 0){
+                echo 'Price is not a positive number or equalto zero!';
+                exit;
+             }  
+
+            if ($qty <= 0){
+                echo 'Quantity is not a positive number or equalto zero!';
+                exit;
+             }              
 
                  //localhost:8080/?product=antiseptic&price=50&qty=2&discount=30 
         ?>        
@@ -28,14 +39,25 @@
                     <th>Unit Cost</th>
                     <th>Quantity</th>
                     <th>Total</th>
-                    <th>Total with discount</th>
+                <?php 
+                        if ($discount >0){
+                            echo '<th>Total with discount</th>';
+                                                                                                
+} ?>
+                    
                 </tr>
                 <tr>
                     <td><?php echo $product ?></td>
                     <td><?php echo $price ?></td>
                     <td><?php echo $qty ?></td>
                     <td><?php echo $total ?></td>
-                    <td><?php echo $twd ?></td>
+                <?php 
+                        if ($discount >0){
+                            echo '<td>'.$twd.'</td>';
+                                                                                                
+} ?>
+                    
+                    
                 </tr>
 
             </table>
