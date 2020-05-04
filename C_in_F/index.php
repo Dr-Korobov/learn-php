20a0a3aacd561fe73d0821ad2db98e5ea8c08ad6
Danil Korobov <dkorobov555@gmail.com>
2020-05-04 20:31:36 +0300

    HTML

--- C_in_F/index.php
+++ C_in_F/index.php
@@ -1,23 +1,37 @@
+<!DOCTYPE html>
 <html>
-    <head></head>    
+    <head>
+            <meta charset="utf-8"/>
+            <title>C_to_F</title>  
+    </head>
     <body>
-        <?php
-            $C=$_GET['c'];
-            $F=$_GET['f'];
+         <form method='POST'>
+            <label>&deg;C</label>
+            <input type="text" name="c" value= <?php echo $C;?>>
+            <button type="submit">Convert</button>
+            <label>&deg;F</label>
+            <input type="text" name="f" value= <?php echo $F; ?>>
+         </form>
+                <?php
+            $C=$_POST['c'];
+            $F=$_POST['f'];
 
             if ($F== null){
                 $F= $C*1.8+32;
                 echo $F;
-                exit;
+                
             }
             if ($C== null){
                 $C= ($F-32)/1.8;
                 echo $C;
-                exit;
             }
             
             
-        ?>        
+        ?> 
+            
+
     </body>
-<html>
+ </html>
+       
+
 
